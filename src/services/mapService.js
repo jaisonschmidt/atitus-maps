6235517ea83a713cf496e2cf44f1958d65dfac7a
Jaison Schmidt <31486070+jaisonschmidt@users.noreply.github.com>
2025-11-07 11:18:24 -0300

    Update BASE_URL in mapService.js

--- src/services/mapService.js
+++ src/services/mapService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://passing-agatha-atitus-0ca94c8f.koyeb.app/ws/point';
+const BASE_URL = 'https://ripe-donella-atitus-fbbf314a.koyeb.app/ws/point';
 
 
 
